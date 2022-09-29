8cbc7aa2567d0a41936d931ff8bfaf9e6966e922
AechtRob <aecht_rob@outlook.com>
2022-09-29 23:53:38 +0100

    Initial commit

---
+++ src/main/java/net/pnprecambrian/ClientProxyPNPrecambrianMod.java
@@ -0,0 +1,25 @@
+package net.pnprecambrian;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+public class ClientProxyPNPrecambrianMod implements IProxyPNPrecambrianMod {
+    @Override
+    public void init(FMLInitializationEvent event) {
+
+    }
+
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @Override
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+
+    @Override
+    public void serverLoad(FMLServerStartingEvent event) {
+    }
+}
---
+++ src/main/java/net/pnprecambrian/IProxyPNPrecambrianMod.java
@@ -0,0 +1,17 @@
+package net.pnprecambrian;
+
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+
+public interface IProxyPNPrecambrianMod {
+    void preInit(FMLPreInitializationEvent event);
+
+    void init(FMLInitializationEvent event);
+
+    void postInit(FMLPostInitializationEvent event);
+
+    void serverLoad(FMLServerStartingEvent event);
+}
